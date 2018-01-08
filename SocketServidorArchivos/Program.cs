8b4b470aa144ab31aca1c35f6a423eac5adb17b9
alex yungan <yungan@hotmail.es>
2018-01-07 19:49:57 -0500

    Add project files.

---
+++ SocketServidorArchivos/Program.cs
@@ -0,0 +1,20 @@
+﻿using LN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServidorArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Datos");
+            
+            Console.Write(LNUsuarios.Instance.Listar().Count);
+            Console.Read();
+        }
+    }
+}
